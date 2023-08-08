ca4785ba5914c3e091828bdd9da5f9ff0982e65a
ARTGonzaXxx <127049897+ARTGonzaXxx@users.noreply.github.com>
2023-08-07 22:05:30 -0300

    Update script.js

--- ex18/script.js
+++ ex18/script.js
@@ -1,4 +1,6 @@
 function vai() {
+
+    
     x = 0
     y = 0
     p = document.querySelector("p")
@@ -6,11 +8,11 @@ function vai() {
         x++
         do {
             y++
-            if (x % 2 == 0) {
-                p.innerHTML += `<span style="color:blue">${x} x ${y} = ${x*y} <br/></span>`
+            if (x  % 2 == 0) {
+                p.innerHTML += `<span style="color: blue;">${x} x ${y} = ${x*y} <br/> </span>`
             } else {
-                p.innerHTML += `<span style="color:red">${x} x ${y} = ${x*y} <br/></span>`
-            }            
+                p.innerHTML += `<span style="color: red;">${x} x ${y} = ${x*y} <br/> </span>`
+            }
             
         } while (y < 10)
         p.innerHTML += "<br/>"
